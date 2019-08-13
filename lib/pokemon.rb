6be1eaa69c11e47ec13165237bb86f5ad054398f
Niusha H <niusha.hadziomerovic@gmail.com>
2019-08-13 06:03:52 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -14,9 +14,10 @@ class Pokemon
         VALUES (?, ?, ?)
       SQL
 
-      DB[:conn].execute(sql, self.name, self.type, self.db)
-
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+
+            DB[:conn].execute(sql, self.name, self.type, self.db)
+
   end 
 
 end

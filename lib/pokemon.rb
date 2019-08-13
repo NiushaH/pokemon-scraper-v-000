1532789d1ac6fd6a95baf7e518c6d67a1653315a
Niusha H <niusha.hadziomerovic@gmail.com>
2019-08-13 05:50:55 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,15 +1,8 @@
 class Pokemon
 
 
-  def self.create_table
-    sql =  <<-SQL 
-      CREATE TABLE pokemon (
-        id INTEGER PRIMARY KEY, 
-        name TEXT, 
-        type INTEGER
-        )
-        SQL
-    DB[:conn].execute(sql)
+  def initialize (id=nil, name, type, db=pokemon)
+    
   end 
 
 

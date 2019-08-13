a7a063319d5425c53724e4e97b136fd585e19124
Niusha H <niusha.hadziomerovic@gmail.com>
2019-08-13 05:39:42 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,6 +1,16 @@
 class Pokemon
 
 
+  def self.create_table
+    sql =  <<-SQL 
+      CREATE TABLE pokemon (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        grade INTEGER
+        )
+        SQL
+    DB[:conn].execute(sql)
+  end 
 
 
 

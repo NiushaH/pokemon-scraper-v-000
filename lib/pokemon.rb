731326da2c5be654b9d6437415909e9c7df1ae95
Niusha H <niusha.hadziomerovic@gmail.com>
2019-08-13 06:11:33 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -12,7 +12,12 @@ class Pokemon
     db.execute("INSERT INTO pokemon (name, type) 
                 VALUES (?, ?)", name, type)
   # @id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+  end
 
-  end 
+  def self.find(id) 
+    row = db.find(id)
+    
+  end
+
 
 end
